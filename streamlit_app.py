80885a4743f0449f96e881ef9fcfb2d630146b3f
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2024-09-20 13:47:59 +0000

    neues updata

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 import pandas as pd 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np
 
 st.title('Hallloo')
 
@@ -10,7 +11,28 @@ df = pd.read_csv('data/Bastar Craton.csv')
 
 cat_names = df.columns.tolist()[27:]
 
-el1 = st.selectbox('selext element', cat_names)
-st.write(el1)
+col1, col2 = st.columns(2)
+
+with col1:
+    el1 = st.selectbox('select element', cat_names)
+    el2 = st.selectbox('selext elememt', cat_names)
+
+with col2:
+    fig = plt.figure()
+    plt.scatter(df[el1]/1000, df[el2]/1000)
+    st.pyplot(fig)
+
 
 st.dataframe(df)
+
+
+
+
+tab1, tab2 = st.tabs(["📈 links", "🗃 rechts"])
+data = np.random.randn(10, 1)
+
+tab1.subheader("A tab with informations")
+tab1.line_chart(data)
+
+tab2.subheader("A tab with the data")
+tab2.write(data)
