bf8323a16aab853ce6ea4670576d4a819abd3a1b
Ali-GeoFZ <158022224+Ali-GeoFZ@users.noreply.github.com>
2024-09-20 07:14:17 +0000

    neue Änderung

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st 
+import pandas as pd 
+import matplotlib.pyplot as plt
 
-st.title('hallloo')
+st.title('Hallloo')
+
 st.write('**Data Science Course 2024**')
+
+df = pd.read_csv('data/Bastar Craton.csv')
+
+st.dataframe(df)
+
